d8cc6c9d39f472884900efa4cfb7820f20538911
Anton ALLIOT <anton.alliot@live.se>
2014-11-04 15:30:24 +0100

    modifs

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,20 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 12:33:04 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/04 14:22:06 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/04 15:30:08 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char		*ft_strdup(const char *s1)
 {
 	char	*s2;
-	int		i;
 
-	i = 0;
-	while (s1[i])
-		i++;
-	s2 = (char*)malloc(i * sizeof(char));
-	if (s2 == NULL)
+	s2 = (char*)malloc(ft_strlen(s1) * sizeof(char) + sizeof(char));
+	if (!s2)
 		return (NULL);
-	while (i != 0)
-	{
-		s2[i] = s1[i];
-		i--;
-	}
+	ft_strcpy(s2, s1);
 	return (s2);
 }
