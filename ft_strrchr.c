73a492a95e38ae67fc5b6079db18d111a00578c8
Anton ALLIOT <anton.alliot@live.se>
2014-11-04 18:34:01 +0100

    aa

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,23 +6,25 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 17:57:01 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/04 18:11:23 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/04 18:29:58 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-char		*ft_strchr(const char *s, int c)
+char		*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
-	i = ft_strlen(s);
-	while (i < 0)
+	i = ft_strlen(s) - 1;
+	if (!c)
+		return ((char*)&s[i + 1]);
+	while (i > 0)
 	{
-		if (!(*s))
-			return (NULL);
+		if (s[i] == c)
+			return ((char*)&s[i]);
 		i--;
 	}
-	return ((char*)s);
+	return (NULL);
 }
