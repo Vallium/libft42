cdef24b70b5cd748b34ac0de4ebcf8e1a6aa6083
Anton ALLIOT <anton.alliot@live.se>
2014-11-04 15:19:00 +0100

    aa

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 10:58:17 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/04 12:39:01 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/04 15:15:53 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-size_t			ft_strlen(const char *tab)
+size_t			ft_strlen(const char *s)
 {
 	size_t		i;
 
 	i = 0;
-	while (tab[i] != 0)
+	while (s[i] != 0)
 		i++;
 	return (i);
 }
