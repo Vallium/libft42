5d9fa04455b1107c6e4959ea66c1f29bcd445917
Anton ALLIOT <anton.alliot@live.se>
2014-11-05 15:35:24 +0100

    aa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 15:08:04 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/05 15:03:46 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/05 15:33:27 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_atoi(const char *str);
 char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
 char		*ft_strcpy(char *dst, const char *src);
 char		*ft_strdup(const char *s1);
 char		*ft_strncpy(char *dst, const char *src, size_t n);
