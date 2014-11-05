87d6652b7f0e86c085ee8cf3ca77220ecc596f59
Anton ALLIOT <anton.alliot@live.se>
2014-11-05 14:31:16 +0100

    aa

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 10:17:32 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/05 14:27:17 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/05 14:31:10 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char			*ft_strstr(const char *s1, const char *s2)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == s2[i])
+		if (str[i] == s2[0])
 			if (ft_scan(&str[i], s2))
 				return (&str[i]);
 		i++;
