0f14e11bf3ee07641171be67a08ecb8d0f9b3251
Anton ALLIOT <anton.alliot@live.se>
2014-11-05 17:48:34 +0100

    memmove

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/05 17:29:21 by aalliot           #+#    #+#             */
+/*   Updated: 2014/11/05 17:48:00 by aalliot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void						*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char			*strdst;
+	const unsigned char		*strsrc;
+
+	strdst = (unsigned char*)dst;
+	strsrc = (const unsigned char*)src;
+	if (!len)
+		return (dst);
+	if (strsrc < strdst)
+	{
+		strdst += len;
+		strsrc += len;
+		while (len--)
+			*strdst-- = *strsrc--;
+	}
+	else
+		*strdst++ = *strsrc++;
+	return (dst);
+}
