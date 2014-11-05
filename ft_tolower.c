5e48be340c52555a69c065cba503843daca37a62
Anton ALLIOT <anton.alliot@live.se>
2014-11-05 14:53:30 +0100

    aa

--- ft_tolower.c
+++ ft_tolower.c
@@ -12,5 +12,5 @@
 
 int			ft_tolower(int c)
 {
-	return((c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c);
+	return ((c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -12,5 +12,5 @@
 
 int			ft_toupper(int c)
 {
-	return((c >= 'a' && c <= 'z') ? c - ('a' - 'A') : c);
+	return ((c >= 'a' && c <= 'z') ? c - ('a' - 'A') : c);
 }
