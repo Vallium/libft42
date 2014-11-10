82def2f4a10fe20774c2d0c6927755977614bee4
Anton ALLIOT <anton.alliot@live.se>
2014-11-10 17:09:28 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,43 +6,21 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 15:37:53 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/05 15:43:33 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/10 17:08:57 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		ft_scan(const char *s1, const char *s2)
-{
-	int			i;
-
-	i = 0;
-	while (s2[i])
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 char			*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	int			i;
-	char		*str;
+	size_t		len_s2;
 
-	str = (char*)s1;
+	len_s2 = ft_strlen(s2);
 	if (!*s2)
-		return (str);
-	i = 0;
-	if (n < ft_strlen(s2))
-		return (NULL);
-	while (str[i] && n--)
-	{
-		if (str[i] == s2[0])
-			if (ft_scan(&str[i], s2))
-				return (&str[i]);
-		i++;
-	}
+		return ((char*)s1);
+	while (*s1 && len_s2 <= n--)
+		if(!ft_memcmp(s1++, s2, len_s2))
+			return((char*)s1 - 1);
 	return (NULL);
 }
