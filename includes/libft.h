aebc97ddfe0da74c4a9c146e3992ef1792b59bfd
Anton ALLIOT <anton.alliot@live.se>
2015-01-02 17:12:24 +0100

    ;

--- includes/libft.h
+++ includes/libft.h
@@ -30,7 +30,7 @@ int					ft_isprint(int c);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
 
-int			ft_intlen(int i)
+int					ft_intlen(int i);
 
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
