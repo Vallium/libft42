f7e936bfe88cd511c483489b3143e8951b9d1fa1
Vallium <anton.alliot@gmail.com>
2016-12-21 15:31:30 +0100

    fix lstddelone function when deleting te first or the last element of a double list

--- src/ft_lstddelone.c
+++ src/ft_lstddelone.c
@@ -15,12 +15,19 @@
 
 void		ft_lstddelone(t_lstd **alst, void (*del)(void *, size_t))
 {
+	t_lstd	*next;
+	t_lstd	*prev;
+
 	if (alst)
 	{
-		(*alst)->prev->next = (*alst)->next;
-		(*alst)->next->prev = (*alst)->prev;
+		next = (*alst)->next;
+		prev = (*alst)->prev;
 		(*del)((*alst)->content, (*alst)->content_size);
 		free(*alst);
 		*alst = NULL;
+		if (prev)
+			prev->next = next;
+		if (next)
+			next->prev = prev;
 	}
 }
